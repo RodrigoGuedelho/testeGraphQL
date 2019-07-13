06894c0294c495200fa55a0c175923e30834dc0f
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2019-07-13 20:30:36 -0300

    Foi adicionado componente UserForm;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 import client from "./services/apollo";
 import UserList from "./components/UserList";
+import UserForm from "./components/UserForm";
+
 const App = () => (
   <ApolloProvider client={client}>
-    <UserList />
+    <>
+      <UserForm />
+      <UserList />
+    </>
   </ApolloProvider>
 );
 
